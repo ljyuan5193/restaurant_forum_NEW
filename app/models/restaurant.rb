4321760af6c9f6e819e3a518d45805cfb72e7ab6
laibuc <ljyuan5193@gmail.com>
2017-12-19 12:12:18 +0800

    create comment model and setup relation between restaurant, user and comment

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,5 +2,5 @@ class Restaurant < ApplicationRecord
   validates_presence_of :name,:tel,:address,:opening_hours,:description
   mount_uploader :image, PhotoUploader
   belongs_to :category, optional: true
-  
+  has_many :comments
 end
