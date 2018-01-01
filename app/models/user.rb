90bc99fdc32827bde3eb591ca9e6db1235ec38cc
laibuc <ljyuan5193@gmail.com>
2018-01-02 07:38:20 +0800

    create favorite model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,15 @@
 class User < ApplicationRecord
+  validates_presence_of :name
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-         validates_presence_of :name
          mount_uploader :avatar, FaceImageUploader
          has_many :comments, dependent: :restrict_with_error
          has_many :restaurants, through: :comments
+         has_many :favorites, dependent: :destroy
+         has_many :favorited_restaurants, through: :favorites, source: :restaurant
+
          def admin?
            self.role == "admin"
          end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171223023833) do
+ActiveRecord::Schema.define(version: 20180101232934) do
 
   create_table "add_users", force: :cascade do |t|
     t.string "name"
@@ -30,6 +30,13 @@ ActiveRecord::Schema.define(version: 20171223023833) do
     t.text "content"
     t.integer "restaurant_id"
     t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "favorites", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "restaurant_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
